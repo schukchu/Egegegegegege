83eb47651163eb05aebbc66c2d6350a8a5576fca
Nikolay Ivanov <81824679+schukchu@users.noreply.github.com>
2022-12-24 11:58:54 +0700

    Create kille.py

---
+++ kille.py
@@ -0,0 +1,40 @@
+kil = str(int(input("Введите большое, но не слишком число")))
+county = 0
+cutt = 0
+kill = map(int, input("И снова \n /n"))
+
+if kil[len(kil) - 1] == 0:
+    print("so unoriginal")
+
+for x in range (len(kill)):
+    county = kill[x] + county
+while True:
+    print('ello')
+    if county == 0:
+        print(cutt)
+        break
+    elif county<0:
+        print("How did you d othis")
+    else:
+        county = county // 2
+        cutt += 1
+
+killula = set()
+killula
+
+print(cutt)
+if cutt>= -10:
+    cutt = cutt-1
+    print("oke")
+
+
+with open('1211.txt') as f:
+    numers = [int(x) for x in f]
+    if numers[0] + numers[1] + numers[2] == 15:
+        a = True
+if cutt > (numers[0] + numers[1] + numers[2]):
+    print("Wowwee")
+
+    with open('1112.txt') as f:
+        n=f.readline()
+        print(n)
